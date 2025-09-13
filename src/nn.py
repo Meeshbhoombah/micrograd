fea3dd7e2e91c5b77580bd768ea0a679acc105ea
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-13 09:55:00 -0400

    Started neural network

---
+++ src/nn.py
@@ -0,0 +1,11 @@
+class Neuron:
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        act = sum(wi*xi for wi, xi zip(self.w, x), 0.0) + self.b
+        out = act.tanh()
+        return out
+
