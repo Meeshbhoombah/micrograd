057eee96376f5eaeda2a1f14f864fe3adccf649e
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-13 08:51:02 -0400

    Added backpropagation, multivar chain rule

--- src/engine.py
+++ src/engine.py
@@ -5,6 +5,7 @@ class Value:
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -14,15 +15,51 @@ class Value:
 
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        
+        out._backward = _backward
+
         return out
 
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
         return out
 
     def tanh(self):
         n = self.data
         t = (math.exp(2*n) - 1)/(math.exp(2*n) + 1)
         out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+
+        out._backward = _backward
+
         return out
+
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
 
