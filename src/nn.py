d1da2b67ea837ce7b3606c3b78b3818dcaf91dd8
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-13 10:00:02 -0400

    Added multilayer perceptron

--- src/nn.py
+++ src/nn.py
@@ -16,3 +16,15 @@ class Layer:
         self.neurons = [Neuron(nin) for _ in range(nout)]
         return(outs)
 
+class MLP:
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+
+        return x
+
