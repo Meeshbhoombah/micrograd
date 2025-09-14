166b5af8d8b5229e57adcc3c6be69720f9f9aa31
Meeshbhoombah <rohanmishra2017@gmail.com>
2025-09-14 13:29:57 -0400

    Added parameter extraction

--- src/nn.py
+++ src/nn.py
@@ -9,12 +9,22 @@ class Neuron:
         out = act.tanh()
         return out
 
+    def parameters(self)
+        return self.w + [self.b]
+
 
 class Layer:
 
     def __init__(self, nin, nout):
         self.neurons = [Neuron(nin) for _ in range(nout)]
-        return(outs)
+        return outs[0] if len(outs == 1) else outs
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
 
 class MLP:
 
@@ -25,6 +35,8 @@ class MLP:
     def __call__(self, x):
         for layer in self.layers:
             x = layer(x)
-
         return x
+
+    def parameters(self)
+        return [p for layer in self.layers for p in layer.parameters()]
 
